b3b4be3385d13e0f77eb163e685d55d1df724e95
nasimrifat101 <nasimrifat101@gmail.com>
2023-10-21 04:21:03 +0600

    slider responsive

--- src/Components/Pages/product/Slider.jsx
+++ src/Components/Pages/product/Slider.jsx
@@ -3,7 +3,7 @@
 const Slider = () => {
   return (
     <div className="mx-auto max-w-6xl">
-      <div className="carousel rounded-box mb-20">
+      <div className="carousel rounded-box m-3 lg:m-0 lg:mb-20">
         <div className="carousel-item w-full">
           <img
             src="https://i.postimg.cc/B6zg7bbs/Desktop-1-3.png"
